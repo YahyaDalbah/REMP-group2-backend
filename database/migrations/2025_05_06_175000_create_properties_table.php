c92132f5164e57af46191b6843d3ecc952627c0f
YahyaDalbah <yahya1dalbah@gmail.com>
2025-05-06 21:42:21 +0300

    added migrations

---
+++ database/migrations/2025_05_06_175000_create_properties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained('users');
+            $table->string('title');
+            $table->string('image');
+            $table->string('location');
+            $table->text('description');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->decimal('price', 15, 2);
+            $table->boolean('isForRent')->default(false);
+            $table->boolean('isForSale')->default(false);
+            $table->enum('status', ['available', 'rented', 'sold'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
