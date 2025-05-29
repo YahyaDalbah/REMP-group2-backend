169c869bae05a654737c188b3e11544121667555
YahyaDalbah <yahya1dalbah@gmail.com>
2025-05-29 16:56:23 +0300

    solve conflict

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,8 @@
 
 use App\Http\Controllers\PropertiesController;
 use App\Http\Controllers\TransactionsController;
-<<<<<<< HEAD
 use App\Http\Controllers\AuthController;
-
-=======
 use App\Http\Controllers\ReviewController;
->>>>>>> d48a8e63f9ccdd998b91892fd6a2e61fd464355b
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +14,6 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource("properties", PropertiesController::class);
 Route::apiResource("transactions", TransactionsController::class);
-<<<<<<< HEAD
-
 Route::post('/register', [AuthController::class ,'register']);
 Route::post('/login', [AuthController::class ,'login']);
-=======
 Route::apiResource('reviews', ReviewController::class);
->>>>>>> d48a8e63f9ccdd998b91892fd6a2e61fd464355b
